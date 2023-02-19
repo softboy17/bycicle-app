340f54fd3ce55a98910d030b960e4820fd6d7323
muradil <04072002mu@gmail.com>
2023-02-19 22:34:17 +0600

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import bycicle from './assets/bycicle.svg';
+import ocean from './assets/ocean.svg';
+import back from './assets/background.svg';
+import Modal from "./components/Modal/Modal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const green = '#06d6a0';
+    const [updateBack, setUpdateBack] = useState('');
+    const backgrounds = [ocean, back, green];
+    const [isModal, setIsModal] = useState(true);
+
+    console.log(isModal);
+    // console.log(backgrounds);
+    // console.log('updated', updateBack);
+
+    return (
+        <div>
+            <div
+                onClick={() => setIsModal(!isModal)}
+                style={{
+                    background: `${updateBack.includes('/') ? `url(${updateBack}) no-repeat` : updateBack}`,
+                    transition: 'background 0.5s ease-in-out'
+                }}>
+                <div>
+                    <img style={{width: '450px', height: '250px'}} src={bycicle} alt="bycicle"/>
+                </div>
+            </div>
+            <Modal isOpen={isModal} onClose={() => setIsModal(!isModal)}>
+                <div style={{display: 'flex'}}>
+                    {
+                        backgrounds.map(item => {
+                            console.log(item.includes('/'));
+                            return (
+                                <div>
+                                    {
+                                        item.includes('/') ?
+                                            <img onClick={() => setUpdateBack(item)}
+                                                 style={{width: '250px', height: '100px', background: item}} src={item}
+                                                 alt="back"/>
+                                            :
+                                            <div onClick={() => setUpdateBack(item)}
+                                                 style={{width: '250px', height: '100px', backgroundColor: item}}></div>
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </Modal>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './modal.css';
+
+const Modal = ({ isOpen, onClose, children }) => {
+    const [isClosing, setIsClosing] = useState(false);
+
+    const handleClose = () => {
+        setIsClosing(true);
+        setTimeout(() => {
+            setIsClosing(false);
+            onClose();
+        }, 300);
+    };
+
+    return (
+        <div className={`modal ${isOpen ? 'open' : ''} ${isClosing ? 'closing' : ''}`}>
+            <div className="modal__overlay" onClick={handleClose}></div>
+            <div className="modal__content">
+                {children}
+                <button onClick={handleClose}>Close Modal</button>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
+
